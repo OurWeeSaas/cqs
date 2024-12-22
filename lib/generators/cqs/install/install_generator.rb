54be4d758e597b90769534f552b6cdee5ca96f52
Enrique Comba Riepenhausen <enrique@ecomba.pro>
2024-12-22 12:11:04 +0100

    Some rails stuff

---
+++ lib/generators/cqs/install/install_generator.rb
@@ -0,0 +1,8 @@
+module CQS::Generators
+  class InstallGenerator < ::Rails::Generators::Base
+    source_root File.expand_path("templates", __dir__)
+
+    def create_intializer
+      template "cqs.rb.erb", Rails.root.join("config/intializers/cqs.rb")
+  end
+end
