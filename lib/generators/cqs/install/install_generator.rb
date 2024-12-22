c4c2cf7b9781fe0becc828aac5914db3b807cfc9
Enrique Comba Riepenhausen <enrique@ecomba.pro>
2024-12-22 12:33:18 +0100

    Create directories when installed

--- lib/generators/cqs/install/install_generator.rb
+++ lib/generators/cqs/install/install_generator.rb
@@ -5,5 +5,10 @@ module Cqs::Generators
     def create_intializer
       template "cqs.rb.erb", Rails.root.join("config/initializers/cqs.rb")
     end
+
+    def create_directories
+      empty_directory Rails.root.join("app/commands/command")
+      empty_directory Rails.root.join("app/queries/query")
+    end
   end
 end
